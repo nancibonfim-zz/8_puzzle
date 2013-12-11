941347418d63674a8279c5ddf306ab615d5c6c98
Arlindo Jr <ba.elias@gmail.com>
2013-12-11 14:47:33 -0300

    Começo. >D

---
+++ 8_puzzle/src/pkg8_puzzle/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg8_puzzle;
+
+/**
+ *
+ * @author Arlindo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
